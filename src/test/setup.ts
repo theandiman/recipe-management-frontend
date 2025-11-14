3da5525066e6593ce39556b4b441a0995a49e6d8
Andy Scott <theandiman@gmail.com>
2025-11-14 22:12:02 +0000

    fix: update package-lock.json and fix failing tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -7,6 +7,10 @@ vi.mock('framer-motion', () => ({
   motion: {
     div: ({ children, ...props }: any) => React.createElement('div', props, children),
     button: ({ children, ...props }: any) => React.createElement('button', props, children),
+    svg: ({ children, ...props }: any) => React.createElement('svg', props, children),
+    path: ({ children, ...props }: any) => React.createElement('path', props, children),
+    h3: ({ children, ...props }: any) => React.createElement('h3', props, children),
+    p: ({ children, ...props }: any) => React.createElement('p', props, children),
   },
   AnimatePresence: ({ children }: any) => React.createElement(React.Fragment, null, children),
 }))
